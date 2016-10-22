47802bac186734c366e41faafc344637f52a7136
Anders Johansson <anjohan@uio.no>
2016-10-22 20:08:14 +0200

    Oppdatering

--- FYS3150/Oblig3/errorearthsun.sh
+++ FYS3150/Oblig3/errorearthsun.sh
@@ -1,8 +1,8 @@
 touch error.dat
 for i in `seq $1 $2`; do
     n=$[10**$i]
-    python python_ui.py -m euler -f euler$n.dat -tn 1 -n $n -dn $n -b sun 1 0 0 0 0 0 0 -b earth 3E-6 1 0 0 0 6.28318530718 0 &
-    python python_ui.py -m verlet -f verlet$n.dat -tn 1 -n $n -dn $n -b sun 1 0 0 0 0 0 0 -b earth 3E-6 1 0 0 0 6.28318530718 0 &
+    python python_ui.py -m euler -f euler$n.dat -tn 1 -n $n -dn $n -s -b earth 3E-6 1 0 0 0 6.28318530718 0 &
+    python python_ui.py -m verlet -f verlet$n.dat -tn 1 -n $n -dn $n -s -b earth 3E-6 1 0 0 0 6.28318530718 0 &
 done
 wait
 for i in `seq $1 $2`; do
