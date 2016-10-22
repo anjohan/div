95ec1806a4e37940f4336e2973a47d3b9d48a281
Anders Johansson <anjohan@uio.no>
2016-10-22 21:31:31 +0200

    oppdatering

---
+++ FYS3150/Gjennomgang3009/vec3.cpp
@@ -0,0 +1,33 @@
+#include "vec3.h"
+#include <cmath>
+
+vec3::vec3(){
+    components[0] = 0;
+    components[1] = 0;
+    components[2] = 0;
+}
+
+vec3::vec3(double x, double y, double z){
+    components[0] = x;
+    components[1] = y;
+    components[2] = z;
+}
+
+void vec3::operator+=(vec3 rhs){
+    components[0] += rhs[0];
+    components[1] += rhs[1];
+    components[2] += rhs[2];
+}
+
+vec3 vec3::operator+(vec3 rhs){
+    vec3 nyvektor = *this; //Lager kopi
+    nyvektor += rhs;
+    return nyvektor;
+}
+
+double vec3::lengthSquared(){
+    return x()*x() + y()*y() + z()*z();
+}
+double vec3::length(){
+    return sqrt(lengthSquared());
+}
---
+++ FYS3150/Gjennomgang3009/vec3.h
@@ -0,0 +1,23 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+class vec3
+    {
+    public:
+        double components[3];
+        vec3();
+        vec3(double x, double y, double z);
+
+        double x(){return components[0];}
+        double y(){return components[1];}
+        double z(){return components[2];}
+
+        double lengthSquared();
+        double length();
+        double operator()(int indeks){return components[indeks];}
+        double operator[](int indeks){return components[indeks];}
+        void operator+=(vec3 rhs);
+        vec3 operator+(vec3 rhs);
+    };
+
+#endif
