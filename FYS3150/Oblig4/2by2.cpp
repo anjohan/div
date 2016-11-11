c5c7dcdc0374465ca02ad3e46ed6882e23a5c0de
Anders Johansson <anjohan@uio.no>
2016-11-11 18:22:07 +0100

    Småfiks

--- FYS3150/Oblig4/2by2.cpp
+++ FYS3150/Oblig4/2by2.cpp
@@ -6,15 +6,19 @@ using namespace std;
 int main(){
     FILE* file1 = fopen("2by2.dat","w");
     FILE* file2 = fopen("2by2long.dat","w");
+    FILE* file3 = fopen("2by2hot.dat","w");
 
     // ising(file1,2,10000000,1000,1.0);
     // ising(file2,2,10000000,1000,1.0);
 
-    thread quick(ising,file1,2,100000,1000,1.0);
-    thread slow(ising,file2,2,10000000,1000,1.0);
+    thread quick(ising,file1,2,100000,1,1.0);
+    thread slow(ising,file2,2,10000000,1,1.0);
+    thread quickhot(ising,file3,2,100000,1,2);
     quick.join();
     slow.join();
+    quickhot.join();
 
     fclose(file1);
     fclose(file2);
+    fclose(file3);
 }
