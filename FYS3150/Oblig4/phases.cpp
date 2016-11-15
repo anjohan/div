95cc32a1ceed53ce61c853a4161c7906e6ad1b7f
Anders Johansson <anjohan@uio.no>
2016-11-15 17:34:18 +0100

    Upload phase.cpp:q

--- FYS3150/Oblig4/phases.cpp
+++ FYS3150/Oblig4/phases.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    if(argc < 3){
+    if(argc < 4){
         printf("Too few arguments!\n");
         return 1;
     }
-    int number_of_temperatures = atoi(argv[1]);
-    int number_of_parallels = atoi(argv[2]);
+    const char* outfilename = argv[1];
+    int number_of_temperatures = atoi(argv[2]);
+    int number_of_parallels = atoi(argv[3]);
 
-    int N = 10000, dN = N+13, startindex=0;
-    double Tstart = 2.0, Tend = 2.3;
+    int N = 100000, dN = N+13, startindex=20000;
+    double Tstart = 2.2, Tend = 2.4;
     double dT = (Tend-Tstart)/(number_of_temperatures-1);
 
     int number_of_sizes = 4;
@@ -72,13 +73,35 @@ int main(int argc, char* argv[]){
         delete [] files[i];
         delete [] values[i];
     }
+
+    FILE* outfile = fopen(outfilename,"w");
+    fprintf(outfile,"$T$");
+    for(i=0; i<number_of_sizes; i++){
+        for(j=0; j<number_of_values; j++){
+            fprintf(outfile," $L=%d$",Lvals[i]);
+        }
+    }
+    fprintf(outfile,"\n");
+    for(j=0; j<number_of_temperatures; j++){
+        fprintf(outfile,"%g",T_vals[j]);
+        for(i=0; i<number_of_sizes; i++){
+            for(k=0; k<number_of_values; k++){
+                fprintf(outfile," %g",resvalues[i][j][k]);
+            }
+        }
+        fprintf(outfile,"\n");
+    }
+
+
+
+    fclose(outfile);
     for(i=0; i<number_of_sizes; i++){
         for(j=0; j<number_of_temperatures; j++){
-            delete [] values[i][j];
+            delete [] resvalues[i][j];
         }
-        delete [] values[i];
+        delete [] resvalues[i];
     }
-    delete [] values;
+    delete [] resvalues;
 
     delete [] T_vals;
 }
