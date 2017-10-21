45397a925e3a08012b7f46ebd4c1e333d7d5ba6f
anjohan <anjohan@uio.no>
2017-10-21 12:47:55 +0200

    Vimting

---
+++ vimplugins.sh
@@ -0,0 +1,21 @@
+set -e -x
+cd ~
+sudo apt-get install libclang
+wget https://github.com/Valloric/ycmd/blob/master/cpp/ycm/.ycm_extra_conf.py
+
+cd .vim
+mkdir pack
+cd pack
+mkdir my-plugins
+cd my-plugins
+mkdir start
+cd start
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.py --clang-completer
+cd ..
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/vim-airline/vim-airline.git
