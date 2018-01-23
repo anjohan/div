4299bb835755e95c02c22b39e33f38aac6d723e2
anjohan <anjohan@uio.no>
2018-01-23 19:29:52 +0100

    Opp

--- vimplugins.sh
+++ vimplugins.sh
@@ -1,6 +1,5 @@
 set -e -x
 cd ~
-wget https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py -O ~/.ycm_extra_conf.py
 
 
 sudo pacman -Syu clang
@@ -13,11 +12,7 @@ mkdir my-plugins
 cd my-plugins
 mkdir start
 cd start
-git clone https://github.com/Valloric/YouCompleteMe.git
-cd YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer --system-libclang
-cd ..
+git clone https://github.com/maralla/completor.vim.git
 git clone https://github.com/w0rp/ale.git
 git clone https://github.com/majutsushi/tagbar.git
 git clone https://github.com/airblade/vim-gitgutter.git
